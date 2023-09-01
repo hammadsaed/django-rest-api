bf8e975fe8840dc27bcff29bd55ba3a24e675d93
Hammad Saeed <hammad.saeed776@gmail.com>
2023-09-01 07:41:49 +0500

    Added code for users list in dashboard

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -31,3 +31,11 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """test that the edit user page works."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        # /admin/core/user/1
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
