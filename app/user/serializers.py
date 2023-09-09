cf2294838de8d7d782c8f693458ab3119bf7254c
Hammad Saeed <hammad.saeed776@gmail.com>
2023-09-10 04:13:15 +0500

    Adds manager user api and tests

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,8 +1,6 @@
 """
 Serializers for the user API View.
 """
-from django.contrib.auth import get_user_model
-
 from rest_framework import serializers
 from django.contrib.auth import (
     get_user_model,
@@ -22,6 +20,18 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update and return a user with encrypted password."""
+        # password is removed from validated_data and assigned to a variable
+        password = validated_data.pop('password', None)
+        # super() calls the ModelSerializer's update() method
+        user = super().update(instance, validated_data)
+        # if password exists, set the password for the user
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
